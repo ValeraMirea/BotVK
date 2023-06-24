7c3f5830fc4c253e38f6d766d39bfa22d85cc8ad
AlexeyAleksandrov <chinok73@yandex.ru>
2023-06-25 02:15:25 +0300

    Подключена база данных

---
+++ DataBase/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bot.DataBase
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<VkUser> VkUsers { get; set; }
+
+        public ApplicationContext()
+        {
+            Database.EnsureCreated(); // проверяем наличие таблиц базы данных
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(
+                "Host=localhost;Port=5432;Database=VkBot;Username=postgres;Password=1111");
+        }
+    }
+}
---
+++ DataBase/VkUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Bot.DataBase
+{
+    public class VkUser
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; } = "";
+        public double Rating { get; set; } = 0.0;
+        public int Warnings { get; set; } = 0;
+        public bool Ban { get; set; } = false;
+    }
+}
